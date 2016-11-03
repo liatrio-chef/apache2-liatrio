9221530f0b652827a5a5ca8c973e8d0789d754f9
Justin Bankes <jbankes@mail.csuchico.edu>
2016-11-03 15:53:52 -0700

    LTA-59 Added source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,10 @@ maintainer_email 'drew@liatrio.com'
 license          'All rights reserved'
 description      'Installs/Configures apache2-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
+
+source_url 'https://github.com/liatrio-chef/apache2-liatrio'
+issues_url 'https://github.com/liatrio-chef/apache2-liatrio/issues'
 
 depends 'java'
 depends 'apache2', '~> 3.2.0'
