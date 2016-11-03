22af8603322083385917c9d1571198a84db49276
Justin Bankes <jbankes@mail.csuchico.edu>
2016-11-03 12:04:12 -0700

    LTA-59 Vagrantfile update; metadata update; testing precommit hook

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,5 @@ description      'Installs/Configures apache2-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
+depends 'java'
 depends 'apache2', '~> 3.2.0'
