f08d2a40d7b0aae110235c61fab04f12f3dc6f24
Drew Holt <drew@liatrio.com>
2016-11-17 21:57:55 -0700

    THEIA-117 using apache2 wrapper cookbook for test, added dependency for 3.2.2 apache2

--- metadata.rb
+++ metadata.rb
@@ -10,4 +10,4 @@ source_url 'https://github.com/liatrio-chef/apache2-liatrio'
 issues_url 'https://github.com/liatrio-chef/apache2-liatrio/issues'
 
 depends 'java'
-depends 'apache2', '~> 3.2.0'
+depends 'apache2', '~> 3.2.2'
